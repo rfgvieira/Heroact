2123be6e050ca82a6c102d5054c93a9cd03180ad
rfgvieira <rfgvieira.01@gmail.com>
2023-11-09 19:30:12 -0300

    feat: banner added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/s</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <header className="banner">
+      <Banner />
       </header>
-    </div>
+      
   );
 }
 
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import "./Banner.css"
+
+
+function Banner(){
+    return(
+        <img src="/images/banner.png" alt="Banner principal"/>
+    )
+}
+
+export default Banner
